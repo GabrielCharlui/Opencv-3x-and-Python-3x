73e50bbcdffb3bcb1df1d6f929656d163f9c0788
Gabrielcharlui <gabrielcharlui@hotmail.com>
2018-09-01 22:24:27 -0300

    update code

--- Passo-a-Passo Projeto/Balanca_Divino_Gramas_LCD_1.0_OK_TCC.ino
+++ Passo-a-Passo Projeto/Balanca_Divino_Gramas_LCD_1.0_OK_TCC.ino
@@ -7,12 +7,14 @@ HX711 scale(A1, A0);    // parameter "gain" is ommited; the default value 128 is
 
 #include <LiquidCrystal.h>
 /*
+Pinos LCD  --> Pinos Arduino
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 LCD pin 4 RS --> pin 8
-LCD pin 6 E --> pin 9
-LCD pin 11 DB4 --> pin 10
-LCD pin 12 DB5 --> pin 11
-LCD pin 13 DB6 --> pin 12
-LCD pin 17 DB7 --> pin 13
+LCD pin 6 E --> pin 7
+LCD pin 11 DB4 --> pin 6
+LCD pin 12 DB5 --> pin 5
+LCD pin 13 DB6 --> pin 4
+LCD pin 17 DB7 --> pin 3
 */
 LiquidCrystal lcd(8, 7, 6, 5, 4, 3);
 //LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
